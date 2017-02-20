d290308a5cbf654c2152fbc8fff1dab0d5be63e0
EnshaednHiker <nbingham09@gmail.com>
2017-02-19 19:54:42 -0600

    beautified code in all affected files

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 function getWords(rawString) {
-  return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+    return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
 }
 
-function analyzeText(){
+function analyzeText() {
     var wordBlock = "";
     var wordArray = [];
-    
-    $("#text-area-box").submit(function(){
+
+    $("#text-area-box").submit(function () {
         event.preventDefault();
         $("#js-word-count,#js-unique-word-count,#js-average-word-length").empty();
-        wordBlock = $('textarea#js-user-text').val(); 
+        wordBlock = $('textarea#js-user-text').val();
         wordArray = getWords(wordBlock);
         $(".text-report").removeClass("hidden");
         $("#js-word-count").append(countWords(wordArray));
         $("#js-unique-word-count").append(countUniqueWords(wordArray));
-        $("#js-average-word-length").append(averageWordLength(wordArray));         
+        $("#js-average-word-length").append(averageWordLength(wordArray));
     });
 
 }
 
-function countWords(array){
+function countWords(array) {
     return array.length;
 }
 
-function countUniqueWords(array){
+function countUniqueWords(array) {
     var arrayUnique = [];
-    array.forEach(function(word, element){
-        if (arrayUnique.includes(word)===false){
+    array.forEach(function (word, element) {
+        if (arrayUnique.includes(word) === false) {
             arrayUnique.push(word);
         }
     });
     return arrayUnique.length;
 }
 
-function averageWordLength(array){
+function averageWordLength(array) {
     var sumLength = 0;
-     
-    array.forEach(function(word){
-       sumLength += word.length;
+
+    array.forEach(function (word) {
+        sumLength += word.length;
     });
-    return sumLength/array.length;
+    return sumLength / array.length;
 }
 
 $(analyzeText)

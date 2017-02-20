7152e34e09518f81c3f7be9862b2190fa4c67a9c
EnshaednHiker <nbingham09@gmail.com>
2017-02-19 18:55:37 -0600

    get average word length fucnctionality working

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ function getWords(rawString) {
 function analyzeText(){
     var wordBlock = "";
     var wordArray = [];
+    $('textarea#js-user-text').click().empty();
+    
     $("#text-area-box").submit(function(){
         event.preventDefault();
         $("#js-word-count,#js-unique-word-count,#js-average-word-length").empty();
@@ -12,6 +14,8 @@ function analyzeText(){
         wordArray = getWords(wordBlock);
         $(".text-report").removeClass("hidden");
         $("#js-word-count").append(countWords(wordArray));
+
+        $("#js-average-word-length").append(averageWordLength(wordArray));
 
          
     });
@@ -24,6 +28,15 @@ return array.length;
 
 function countUniqueWords(array){
     array
+}
+
+function averageWordLength(array){
+    var sumLength = 0;
+     
+    array.forEach(function(element){
+       sumLength += element.length;
+    });
+    return sumLength/array.length;
 }
 
 $(analyzeText)
